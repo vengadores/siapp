0e58cd827b5562ddb70a788a0c6b05952601ef69
Angel <eduardo.aemc@gmail.com>
2016-07-15 16:55:03 +0000

    Validando

--- siapp.rb
+++ siapp.rb
@@ -5,3 +5,11 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
+
+post '/send_data' do
+    if params["email"].include? "@"
+        "Este correo es valido"
+    else
+        "Este correo es invalido"
+    end
+end
